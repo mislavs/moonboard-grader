0ce59ae62598018c2d12a337ae2f05e725c67e9e
Mislav Starzik <mislav.starzik@gmail.com>
2025-11-12 13:18:41 +0100

    Don't generate a log file

--- generator/main.py
+++ generator/main.py
@@ -22,8 +22,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler('generator.log')
+        logging.StreamHandler(sys.stdout)
     ]
 )
 logger = logging.getLogger(__name__)
